1f5df057d48731106a44b3d190e9f9dca95a1b5a
Alexandro Tapia <zpaceway@gmail.com>
2023-04-24 15:54:17 -0500

    fix spinner position

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -3,7 +3,7 @@ import { CgSpinner } from "react-icons/cg";
 const LoadingScreen = () => {
   return (
     <div className="absolute inset-0 flex items-center justify-center">
-      <div className="h-20 w-20 overflow-hidden">
+      <div className="flex h-20 w-20 items-center justify-center overflow-hidden">
         <CgSpinner className="animate-spin text-6xl text-blue-500" />
       </div>
     </div>
